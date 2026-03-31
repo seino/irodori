6a286fae2da0a6530aa5f859b3446b1b91704fb9
Akihiro Seino <akihiro@seino.biz>
2026-03-31 22:54:06 +0900

    chore: disable sourcemap in tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   format: ['esm', 'cjs'],
   dts: true,
   splitting: false,
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
   minify: false,
   target: 'node18',
